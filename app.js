a2d305bd2c090b0f474024d97374dcc4cd2d16fa
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-27 20:22:58 +0300

    Решение 4.6 колбеки

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-// функция высшего порядка, принимает функцию в параметрах
-function copyArrayAndDoSmth(arr, instruction) {
-  const output = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    output.push(instruction(arr[i]));
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
   }
-
-  return output;
-}
-// функция - колбек
-function numSquared(num) {
-  return num ** 2;
+  return newArray;
 }
 
-// функция - колбек
-function divideBy2(num) {
-  return num / 2;
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+function isEven(num) {
+  return num % 2 === 0;
 }
 
-const result = copyArrayAndDoSmth([7, 8, 9], numSquared);
-const result2 = copyArrayAndDoSmth([10, 20, 30], divideBy2);
-
+const result = filterArray([1, 2, 3, 4, 5], isEven);
 console.log(result);
-console.log(result2);
