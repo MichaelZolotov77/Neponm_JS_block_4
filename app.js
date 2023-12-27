f531b459f91eac49c5aa5aba8e8523d0701993d5
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-27 23:06:41 +0300

    Мое решение задачки на рекурсию

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-function factorial(n) {
-  if (n === 0) return 1;
-
-  return n * factorial(n - 1);
-}
-
-console.log(factorial(5));
-
 let counter = 0;
-// принимает символ и возвращает этот символ в пяти экземплярах
-function repeater(char) {
+function getLength(arr) {
   counter++;
-
-  if (counter === 5) {
-    counter === 0;
-    return char;
-  }
-
-  return char + repeater(char);
+  if (!arr.pop()) return counter - 1;
+  return getLength(arr);
 }
 
-console.log(repeater("a2"));
+getLength(arr);
