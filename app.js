0d9aa9fd1f0d709176744f7c443ecdca5ad1c4c5
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-28 13:22:33 +0300

    Примеры замыканий от преподавателя

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-function addByX(x) {
-  return (y) => x + y;
+// Closure
+// 1. Функция возвращает функцию
+// 2. Возвращаемая функция запоминает scope(окружение)
+
+function hello(name) {
+  const helloName = () => console.log("Hello", name);
+
+  return helloName; // Важно! Здесь функция не вызывается.
 }
 
-const addByTwo = addByX(2);
+const helloWorld = hello("world");
+helloWorld(); //Hello world
 
-console.log(addByTwo(3)); // 5
-console.log(addByTwo(5)); // 7
+const helloJohn = hello("John");
+helloJohn(); //Hello John
+
+function outer() {
+  let counter = 0;
+
+  function incrementCounter() {
+    console.log(++counter);
+  }
+
+  return incrementCounter;
+}
+
+// Два независимых счетчика
+const myCounter1 = outer();
+myCounter1(); // 1
+myCounter1(); // 2
+myCounter1(); // 3
+
+const myCounter2 = outer();
+myCounter2();
+myCounter2();
+myCounter2();
