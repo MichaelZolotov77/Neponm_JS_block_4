ac8fb9cd28492c37e3211ed6aa0a1b4dca68a759
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-29 11:39:20 +0300

    Задачка на контекст 4.10

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-//this
+const supporter1 = {
+  club: "Milan",
 
-const developer = {
-  name: "Mikhail",
-  salary: 2500,
-
-  getBonus(sum1, sum2) {
-    console.log(
-      this.name,
-      "has a salary this month",
-      this.salary + sum1 + sum2
-    );
+  chant() {
+    setTimeout(() => console.log("Forza", this.club), 1000);
   },
 };
 
-// developer.getBonus(250);
-
-const manager = {
-  name: "Anna",
-  salary: 1800,
+const supporter2 = {
+  club: "Inter",
 };
 
-// при вызове метода call можно подставить другой контекст
-// хотя вызывали Михаила, получили Анну
-developer.getBonus.call(manager, 150, 200);
+supporter1.chant();
 
-// делает то же самое, только принимает параметры в виде массива
-developer.getBonus.apply(manager, [150, 200]);
-
-// привязка контекста к менеджеру, создание функции у Анны
-manager.getBonus = developer.getBonus.bind(manager);
-// вызов новой функции
-manager.getBonus(150, 200);
+supporter2.chant = supporter1.chant.bind(supporter2);
+supporter2.chant();
